32ce100d9aee0cce83024eecb6f55fe8c45f0c40
LongLe <xuanlongvts@gmail.com>
2022-05-09 16:23:26 +0700

    add footer

--- fe/src/_commComp/footer/index.tsx
+++ fe/src/_commComp/footer/index.tsx
@@ -1,5 +1,38 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Footer = () => {
-    return <footer>&copy; xuanlongvts - 05/2022</footer>;
+    return <footer id="footerSlim">&copy; xuanlongvts - 05/2022</footer>;
 };
 
 export default Footer;
+
+export const FooterHome = () => {
+    return (
+        <footer id="footerFull">
+            <div className="author">&copy; xuanlongvts - 05/2022</div>
+            <div className="logos">
+                <Link href="https://solana.com/">
+                    <a target="_blank" rel="noopener">
+                        <Image src="/imgs/sol.svg" alt="Solana" width={40} height={25} />
+                    </a>
+                </Link>
+                <Link href="https://sentre.io/">
+                    <a target="_blank" rel="noopener">
+                        <Image src="/imgs/logo_sentre.png" alt="Sentre" width={85} height={25} />
+                    </a>
+                </Link>
+                <Link href="https://coin98.net/">
+                    <a target="_blank" rel="noopener">
+                        <Image src="/imgs/logo_coin98.svg" alt="Coin 98" width={72} height={21} />
+                    </a>
+                </Link>
+                <Link href="https://rove.to">
+                    <a target="_blank" rel="noopener">
+                        <Image src="/imgs/logo_rove.svg" alt="Rove" width={50} height={25} />
+                    </a>
+                </Link>
+            </div>
+        </footer>
+    );
+};
